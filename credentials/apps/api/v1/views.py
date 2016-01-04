697a445d15b74aad87e03907b8216bf4b9f244e5
Tasawer <tasawer.nawaz@arbisoft.com>
2016-01-04 13:08:34 +0500

    ECOM-3263 added filtering for course and programs credentials

--- credentials/apps/api/v1/views.py
+++ credentials/apps/api/v1/views.py
@@ -3,9 +3,10 @@ Credentials service API views (v1).
 """
 import logging
 
-from rest_framework import filters, viewsets
+from rest_framework import filters, mixins, viewsets
 from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
+from credentials.apps.api.filters import ProgramFilter, CourseFilter
 
 from credentials.apps.api.serializers import UserCredentialCreationSerializer, UserCredentialSerializer
 from credentials.apps.credentials.models import UserCredential
@@ -33,3 +34,35 @@ class UserCredentialViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         self.serializer_class = UserCredentialCreationSerializer
         return super(UserCredentialViewSet, self).create(request, *args, **kwargs)
+
+
+class ProgramsCredentialsViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """It will return the all credentials for programs."""
+    queryset = UserCredential.objects.all()
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = ProgramFilter
+    serializer_class = UserCredentialSerializer
+
+    def list(self, request, *args, **kwargs):
+        if not self.request.query_params.get('program_id'):
+            raise ValidationError(
+                {'error': 'A program_id query string parameter is required for filtering program credentials.'})
+
+        # pylint: disable=maybe-no-member
+        return super(ProgramsCredentialsViewSet, self).list(request, *args, **kwargs)
+
+
+class CourseCredentialsViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """It will return the all credentials for courses."""
+    queryset = UserCredential.objects.all()
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_class = CourseFilter
+    serializer_class = UserCredentialSerializer
+
+    def list(self, request, *args, **kwargs):
+        if not self.request.query_params.get('course_id'):
+            raise ValidationError(
+                {'error': 'A course_id query string parameter is required for filtering course credentials.'})
+
+        # pylint: disable=maybe-no-member
+        return super(CourseCredentialsViewSet, self).list(request, *args, **kwargs)
