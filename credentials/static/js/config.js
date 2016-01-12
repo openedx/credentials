b4901fb27899e2498d4b6f98eab8bd2d7759e701
Awais <awais.qureshi@arbisoft.com>
2016-01-12 15:08:36 +0500

    Adding sass files and implementing UX pattern lib. ECOM-3417

--- credentials/static/js/config.js
+++ credentials/static/js/config.js
@@ -5,6 +5,14 @@ require.config({
         'jquery': 'bower_components/jquery/dist/jquery',
         'requirejs': 'bower_components/requirejs/require',
         'text': 'bower_components/text/text',
-        'underscore': 'bower_components/underscore/underscore'
+        'underscore': 'bower_components/underscore/underscore',
+        'afontgarde': 'bower_components/edx-pattern-library/pattern-library/js/afontgarde',
+        'modernizr-custom': 'bower_components/edx-pattern-library/pattern-library/js/modernizr-custom'
+    },
+    shim: {
+        'afontgarde' : {
+            deps: ['modernizr-custom'],
+            exports: 'AFontGarde'
+        }
     }
 });
