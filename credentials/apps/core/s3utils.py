fe03b8e7fdc3dd47d421d242152dc589da8cf996
zubair-arbi <zubair.afzal@arbisoft.com>
2016-02-16 16:55:49 +0500

    add S3BotoStorage for static and media files

---
+++ credentials/apps/core/s3utils.py
@@ -0,0 +1,18 @@
+"""
+Custom S3 storage backends to store files in sub folders.
+"""
+from functools import partial
+
+from django.conf import settings
+from storages.backends.s3boto import S3BotoStorage
+
+
+MediaS3BotoStorage = partial(
+    S3BotoStorage,
+    location=settings.MEDIA_ROOT.strip('/')
+)
+
+StaticS3BotoStorage = partial(
+    S3BotoStorage,
+    location=settings.STATIC_ROOT.strip('/')
+)
