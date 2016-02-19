5157f1f66ea53e8fadec2f3f05abe016b00337aa
Awais <awais.qureshi@arbisoft.com>
2016-02-19 15:34:29 +0500

    Adding acceptance test ecom-3695

--- acceptance_tests/test_certificate.py
+++ acceptance_tests/test_certificate.py
@@ -1,28 +1,26 @@
 from unittest import skipUnless
 
 from bok_choy.web_app_test import WebAppTest
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 from acceptance_tests.config import ENABLE_OAUTH2_TESTS
 from acceptance_tests.mixins import LoginMixin
-from acceptance_tests.pages import LMSDashboardPage
 
 
 @skipUnless(ENABLE_OAUTH2_TESTS, 'OAuth2 tests are not enabled.')
-class DashboardTests(LoginMixin, WebAppTest):
-    def setUp(self):
-        """
-        Instantiate the page objects.
-        """
-        super(DashboardTests, self).setUp()
-        self.dashboard_page = LMSDashboardPage(self.browser)
+class RenderCredentialTests(LoginMixin, WebAppTest):
 
     def test_student_dashboard_with_certificate(self):
+        """ Load the credentials from student dashboard."""
 
         self.login_with_lms()
-        self.assertTrue(self.browser.find_element_by_css_selector('.title'))
-        self.browser.find_element_by_xpath(".//*[@class='wrapper-xseries-certificates']/ul/li/a").click()
+        link = ".wrapper-xseries-certificates ul li a"
+        WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, link)))
+
+        link_info = self.browser.find_element_by_css_selector(link).get_attribute('href')
+
+        # click is not working so load the credential url in browser.
+        self.browser.get(link_info)
         WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.ID, 'action-print-view')))
