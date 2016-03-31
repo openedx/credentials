a14af0be5a442452434dafdd88994d9384503dfe
Ahsan Ulhaq <ahsan.haq@arbisoft.com>
2016-03-31 15:36:53 +0500

    Add signatory organization info ECOM-4009

---
+++ credentials/apps/credentials/migrations/0002_signatory_organization_name_override.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('credentials', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='signatory',
+            name='organization_name_override',
+            field=models.CharField(help_text='Signatory organization name if its different from issuing organization.', max_length=255, null=True, blank=True),
+        ),
+    ]
