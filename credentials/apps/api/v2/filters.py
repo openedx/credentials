a9bb0c3f1388e9457846e0ef0a5554731248d5b2
Clinton Blackburn <clinton.blackburn@gmail.com>
2017-01-11 22:11:56 -0500

    Added Django REST Swagger
    ECOM-6808

--- credentials/apps/api/v2/filters.py
+++ credentials/apps/api/v2/filters.py
@@ -1,10 +1,19 @@
 import django_filters
+from django.utils.translation import ugettext_lazy as _
 
 from credentials.apps.credentials.models import UserCredential
 
 
 class UserCredentialFilter(django_filters.FilterSet):
-    program_uuid = django_filters.UUIDFilter(name='program_credentials__program_uuid')
+    program_uuid = django_filters.UUIDFilter(
+        name='program_credentials__program_uuid',
+        label=_('UUID of the program for which the credential was awarded')
+    )
+    status = django_filters.ChoiceFilter(
+        choices=UserCredential._meta.get_field('status').choices,
+        label=_('Status of the credential')
+    )
+    username = django_filters.CharFilter(label=_('Username of the recipient of the credential'))
 
     class Meta:
         model = UserCredential
---
+++ credentials/apps/credentials/migrations/0006_auto_20170112_0308.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('credentials', '0005_auto_20170111_0441'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='usercredential',
+            name='download_url',
+            field=models.CharField(help_text='URL at which the credential can be downloaded', max_length=255, null=True, blank=True),
+        ),
+    ]
