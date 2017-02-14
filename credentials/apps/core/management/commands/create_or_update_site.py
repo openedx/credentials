66026cc4997c7f386fc2fca6a7735a39c813fa24
Marko Jevtic <marjevtic@gmail.com>
2017-02-14 14:49:29 +0100

    [ECOM-7453] Enable Configuration of a White Label site

---
+++ credentials/apps/core/management/commands/create_or_update_site.py
@@ -0,0 +1,143 @@
+""" Creates or updates a Site including SiteConfiguration data. """
+from __future__ import unicode_literals
+
+import logging
+
+from django.contrib.sites.models import Site
+from django.core.management import BaseCommand
+
+from credentials.apps.core.models import SiteConfiguration
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = 'Create or update Site and SiteConfiguration'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--site-id',
+            action='store',
+            dest='site_id',
+            type=int,
+            help='ID of the Site to update.'
+        )
+        parser.add_argument(
+            '--site-domain',
+            action='store',
+            dest='site_domain',
+            type=str,
+            required=True,
+            help='Domain of the Site to create or update.'
+        )
+        parser.add_argument(
+            '--site-name',
+            action='store',
+            dest='site_name',
+            type=str,
+            required=True,
+            help='Name of the Site to create or update.'
+        )
+        parser.add_argument(
+            '--platform-name',
+            action='store',
+            dest='platform_name',
+            type=str,
+            required=True,
+            help='Name of your Open edX platform.'
+        )
+        parser.add_argument(
+            '--lms-url-root',
+            action='store',
+            dest='lms_url_root',
+            type=str,
+            required=True,
+            help='Root URL of LMS (e.g. https://localhost:8000)'
+        )
+        parser.add_argument(
+            '--catalog-api-url',
+            action='store',
+            dest='catalog_api_url',
+            type=str,
+            required=True,
+            help='Root URL of the Catalog API (e.g. https://api.edx.org/catalog/v1/)'
+        )
+        parser.add_argument(
+            '--tos-url',
+            action='store',
+            dest='tos_url',
+            type=str,
+            required=True,
+            help='Terms of Service URL'
+        )
+        parser.add_argument(
+            '--privacy-policy-url',
+            action='store',
+            dest='privacy_policy_url',
+            type=str,
+            required=True,
+            help='Privacy Policy URL'
+        )
+        parser.add_argument(
+            '--homepage-url',
+            action='store',
+            dest='homepage_url',
+            type=str,
+            required=True,
+            help='Homepage URL'
+        )
+        parser.add_argument(
+            '--company-name',
+            action='store',
+            dest='company_name',
+            type=str,
+            required=True,
+            help='Company Name'
+        )
+        parser.add_argument(
+            '--verified-certificate-url',
+            action='store',
+            dest='verified_certificate_url',
+            type=str,
+            required=True,
+            help='Verified Certificate URL'
+        )
+        parser.add_argument(
+            '--certificate-help-url',
+            action='store',
+            dest='certificate_help_url',
+            type=str,
+            required=True,
+            help='Certificate Help URL'
+        )
+
+    def handle(self, *args, **options):
+        site_id = options.get('site_id')
+        site_domain = options.get('site_domain')
+        site_name = options.get('site_name')
+
+        try:
+            site = Site.objects.get(id=site_id)
+        except Site.DoesNotExist:
+            site, site_created = Site.objects.get_or_create(domain=site_domain)
+            if site_created:
+                logger.info('Created Site [%d] with domain [%s]', site.id, site.domain)
+
+        site.domain = site_domain
+        site.name = site_name
+        site.save()
+
+        SiteConfiguration.objects.update_or_create(
+            site=site,
+            defaults={
+                'platform_name': options.get('platform_name'),
+                'lms_url_root': options.get('lms_url_root'),
+                'catalog_api_url': options.get('catalog_api_url'),
+                'tos_url': options.get('tos_url'),
+                'privacy_policy_url': options.get('privacy_policy_url'),
+                'homepage_url': options.get('homepage_url'),
+                'company_name': options.get('company_name'),
+                'verified_certificate_url': options.get('verified_certificate_url'),
+                'certificate_help_url': options.get('certificate_help_url')
+            }
+        )
--- docs/credentials_admin.rst
+++ docs/credentials_admin.rst
@@ -24,6 +24,84 @@ All fields in the ``SiteConfiguration`` model are required.
 Each site can have only one configuration.
 
 
+Add Another Site and Site Configuration
+-----------------------------------------
+
+If you want to add more sites, partners, and site configurations, you can use
+the ``create_or_update_site`` command. The following options are available for
+this command.
+
+.. list-table::
+   :widths: 25 20 60 20
+   :header-rows: 1
+
+   * - Option
+     - Required
+     - Description
+     - Example
+   * - ``--site-id``
+     - No
+     - Database ID of a site you want to update.
+     - ``--site-id=1``
+   * - ``--site-domain``
+     - Yes
+     - Domain by which you will access
+       the Credentials service.
+     - ``--site-domain=credentials.example.com``
+   * - ``--site-name``
+     - Yes
+     - Name of the Credentials site.
+     - ``--site-name='Example Credentials'``
+   * - ``--platform-name``
+     - Yes
+     - Name of the Open edX Platform.
+     - ``--platform-name='Example Platform name'``
+   * - ``--lms-url-root``
+     - Yes
+     - URL root of the Open edX LMS instance.
+     - ``--lms-url-root=https://example.com``
+   * - ``--catalog-api-url``
+     - Yes
+     - URL root of the Course Catalog API.
+     - ``--catalog-api-url=https://discovery-example.com/api/v1/``
+   * - ``--tos-url``
+     - Yes
+     - URL of the Terms of Service page.
+     - ``--tos-url=https://example.com/tos``
+   * - ``--privacy-policy-url``
+     - Yes
+     - URL of the Privacy Policy page.
+     - ``--privacy-policy-url=https://example.com/privacy-policy``
+   * - ``--homepage-url``
+     - Yes
+     - URL of the Company's Homepage.
+     - ``--homepage-url=https://example.com/``
+   * - ``--company-name``
+     - Yes
+     - Name of the Company which owns the instance.
+     - ``--company-name=Example Company name``
+   * - ``--verified-certificate-url``
+     - Yes
+     - URL of the page with information on verified certificates.
+     - ``--verified-certificate-url=https://example.com/verified-certificate``
+   * - ``--certificate-help-url``
+     - Yes
+     - URL of the page for questions about certificates.
+     - ``--certificate-help-url=https://example.com/web-certificates``
+
+
+To add another site, use the appropriate settings module for your environment
+(``credentials.settings.devstack`` for Devstack,
+``credentials.settings.production`` for Fullstack) to run the following Django
+management command. This command creates a new site, and site
+configuration with the options that you specify.
+
+    .. code-block:: bash
+
+      $ sudo su credentials
+      $ python manage.py create_or_update_site --site-domain=[change me] --site-name=[change me] --platform-name=[change me] --lms-url-root=[change me] --catalog-api-url=[change me] --tos-url=[change me] --privacy-policy-url=[change me] --homepage-url=[change me] --company-name=[change me] --verified-certificate-url=[change me] --certificate-help-url=[change me]
+
+
 Template Configuration  (Optional)
 =====================================
 
