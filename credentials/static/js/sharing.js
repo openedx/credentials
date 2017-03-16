ef77989d41a9707e8efbd855eb2b6130c8cb8bbf
Clinton Blackburn <cblackburn@edx.org>
2017-03-16 15:19:26 -0400

    Facebook sharing updates - Loading sharing.js only if Facebook sharing is enabled - Removed quote from share dialog
    ECOM-6578

--- credentials/static/js/sharing.js
+++ credentials/static/js/sharing.js
@@ -1,26 +1,29 @@
 import $ from "jquery";
 
 $(document).ready(function () {
-    var $shareButton = $('.action-facebook');
+    var facebookAppId = window.edx.facebook.appId;
 
-    $.ajaxSetup({cache: true});
-    $.getScript('//connect.facebook.net/en_US/sdk.js', function () {
-        FB.init({
-            appId: window.edx.facebook.appId,
-            version: 'v2.7'
+    if (facebookAppId) {
+        $.ajaxSetup({cache: true});
+        $.getScript('//connect.facebook.net/en_US/sdk.js', function () {
+            var $shareButton = $('.action-facebook');
+
+            FB.init({
+                appId: facebookAppId,
+                version: 'v2.7'
+            });
+
+            // Activate the sharing button
+            $shareButton.removeAttr('disabled');
+
+            $shareButton.click(function () {
+                FB.ui({
+                    method: 'share',
+                    href: window.edx.facebook.href
+                }, function (response) {
+                    // TODO Log to Segment. The response will be empty since we don't use Facebook login.
+                });
+            });
         });
-
-        // Activate the sharing button
-        $shareButton.removeAttr('disabled');
-    });
-
-    $shareButton.click(function () {
-        FB.ui({
-            method: 'share',
-            href: window.edx.facebook.href,
-            quote: window.edx.facebook.quote,
-        }, function (response) {
-            // TODO Log to Segment. The response will be empty since we don't use Facebook login.
-        });
-    });
+    }
 });
