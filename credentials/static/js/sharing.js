d0b1b35e316bbd145445a027d7b3cb791c63b2a7
Clinton Blackburn <cblackburn@edx.org>
2017-03-20 15:14:27 -0400

    Added ESLint
    - Using ESLint to lint JavaScript code
    - Cleaned linting errors
    
    ECOM-6578

--- credentials/static/js/sharing.js
+++ credentials/static/js/sharing.js
@@ -1,38 +1,29 @@
-let facebookAppId = window.edx.facebook.appId;
+const facebookAppId = window.edx.facebook.appId;
 
-if (facebookAppId) {
-    window.fbAsyncInit = function () {
-        let shareButton = document.querySelector('.action-facebook');
+function addFacebookClickHandler() {
+  'use strict';
 
-        FB.init({
-            appId: facebookAppId,
-            xfbml: true,
-            version: 'v2.8'
-        });
-        FB.AppEvents.logPageView();
-
-        // Activate the sharing button
-        shareButton.removeAttribute('disabled');
-
-        shareButton.addEventListener('click', function () {
-            FB.ui({
-                method: 'share',
-                href: window.edx.facebook.href
-            }, function (response) {
-                // NOTE (CCB): We intentionally do nothing here. The click handler in analytics.js
-                // is responsible for firing analytics events.
-            });
-        });
-    };
-
-    (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {
-            return;
-        }
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+  FB.ui({
+    method: 'share',
+    href: window.edx.facebook.href,
+  });
 }
+
+function initializeFacebook() {
+  'use strict';
+
+  const shareButton = document.querySelector('.action-facebook');
+
+  FB.init({
+    appId: facebookAppId,
+    xfbml: true,
+    version: 'v2.8',
+  });
+  FB.AppEvents.logPageView();
+
+  // Activate the sharing button
+  shareButton.removeAttribute('disabled');
+  shareButton.addEventListener('click', addFacebookClickHandler);
+}
+
+window.fbAsyncInit = initializeFacebook;
