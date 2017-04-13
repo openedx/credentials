49d3037bf7538b0906960a88b5e95e895e2b0c0b
Renzo Lucioni <renzo@lucioni.xyz>
2017-04-13 11:20:38 -0400

    Use pytest to run tests
    Having been in maintenance mode for the past several years, nose is effectively dead. edX and the Python community at large are moving towards pytest. pytest is actively developed and provides several benefits over nose, including detailed introspection of failing assert statements (no need to use unittest's assert methods), a rich plugin ecosystem, and modular fixtures (which may help move us away from relying on migrations for populating initial test data).

--- credentials/settings/test.py
+++ credentials/settings/test.py
@@ -5,24 +5,13 @@ from path import Path as path
 from credentials.settings.base import *
 from credentials.settings.utils import get_logger_config
 
-# TEST SETTINGS
+
 INSTALLED_APPS += [
-    'django_nose',
     'credentials.apps.edx_credentials_extensions',
 ]
 
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-
-NOSE_ARGS = [
-    '--with-ignore-docstrings',
-    '--logging-level=DEBUG',
-]
-# LOGGING
 LOGGING = get_logger_config(debug=False, dev_env=True, local_loglevel='DEBUG')
-# END TEST SETTINGS
 
-
-# IN-MEMORY TEST DATABASE
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -33,15 +22,13 @@ DATABASES = {
         'PORT': '',
     },
 }
-# END IN-MEMORY TEST DATABASE
 
 # Local Directories
-TEST_ROOT = path("test_root")
+TEST_ROOT = path('test_root')
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
-MEDIA_ROOT = TEST_ROOT / "uploads"
-MEDIA_URL = "/static/uploads/"
+MEDIA_ROOT = TEST_ROOT / 'uploads'
+MEDIA_URL = '/static/uploads/'
 
-# AUTHENTICATION
 OAUTH2_PROVIDER_URL = 'https://test-provider/oauth2'
 SOCIAL_AUTH_EDX_OIDC_URL_ROOT = OAUTH2_PROVIDER_URL
 
@@ -50,4 +37,3 @@ JWT_AUTH.update({
     'JWT_ISSUER': OAUTH2_PROVIDER_URL,
     'JWT_AUDIENCE': SOCIAL_AUTH_EDX_OIDC_KEY,
 })
-# END AUTHENTICATION
