890384aef6b3e3d69081957781a55d1ac6d61ded
Michael Terry <mterry@edx.org>
2018-01-12 13:37:14 -0500

    Fix Spanish date translations
    Django's Spanish date formatting uses uppercase months, but Spanish
    speakers expect lowercase months. Workaround Django's current
    behavior and also make sure that translators have more control of
    how the certificate issued-on phrase is formatted.
    
    LEARNER-3121

--- credentials/apps/credentials/templatetags/html.py
+++ credentials/apps/credentials/templatetags/html.py
@@ -2,7 +2,9 @@
 Template tags and helper functions for escaping script tag.
 """
 from django import template
+from django.template.defaultfilters import date
 from django.utils import html
+from django.utils.translation import get_language
 register = template.Library()
 
 
@@ -29,3 +31,26 @@ def interpolate_html(value, **kwargs):
     escaped_msg = html.escape(value)
     safe_interpolated_msg = html.format_html(escaped_msg, **kwargs)
     return safe_interpolated_msg
+
+
+@register.filter(expects_localtime=True, is_safe=False)
+def month(value):
+    """
+    Provides the month from a provided date as a string.
+
+    This is used to work around a bug in the Spanish django month translations.
+    See LEARNER-3859 for more details.
+
+    Arguments:
+        value (datetime): date to format
+
+    Returns:
+        string: A formatted version of the month
+    """
+    formatted = date(value, 'E')
+
+    language = get_language()
+    if language and language.split('-')[0].lower() == 'es':
+        return formatted.lower()
+
+    return formatted
