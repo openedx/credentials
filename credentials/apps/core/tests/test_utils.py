453f82525825900c35aba00560f7173dc10089a2
Michael Terry <mterry@edx.org>
2018-06-20 09:48:58 -0400

    Sync full name on login
    We already synced the username and email, but we also want the full
    name, which we'll display on student records.
    
    LEARNER-5304

---
+++ credentials/apps/core/tests/test_utils.py
@@ -0,0 +1,29 @@
+"""Test core.utils."""
+
+import mock
+from django.test import TestCase
+
+from credentials.apps.core.tests.factories import UserFactory
+from credentials.apps.core.utils import update_full_name
+
+
+class UtilsTests(TestCase):
+    """ Tests for the utility functions."""
+
+    def setUp(self):
+        super(UtilsTests, self).setUp()
+        self.user = UserFactory(full_name='Bart')
+
+    def test_update_full_name_no_user(self):
+        # Just test that we don't blow up
+        update_full_name(mock.MagicMock(), {})
+
+    def test_update_full_name_no_full_name(self):
+        # Just test that we don't blow up
+        update_full_name(mock.MagicMock(), {}, self.user)
+
+    def test_update_full_name_happy_path(self):
+        strategy = mock.MagicMock()
+        update_full_name(strategy, {'full_name': 'Bort'}, self.user)
+        self.assertEqual(self.user.full_name, 'Bort')
+        self.assertTrue(strategy.storage.user.changed.called)
