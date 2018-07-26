12c26b787ffb4638b7f4beaa1815568c95aea0df
Brandon Baker <bbaker@edx.org>
2018-07-26 16:38:15 -0400

    Add rate limiting to credentials records endpoint LEARNER-5925

--- credentials/settings/base.py
+++ credentials/settings/base.py
@@ -80,6 +80,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'social_django.middleware.SocialAuthExceptionMiddleware',
     'waffle.middleware.WaffleMiddleware',
+    'ratelimit.middleware.RatelimitMiddleware',
 )
 
 ROOT_URLCONF = 'credentials.urls'
@@ -380,6 +381,9 @@ REST_FRAMEWORK = {
     'DATETIME_FORMAT': '%Y-%m-%dT%H:%M:%SZ',
     'EXCEPTION_HANDLER': 'credentials.apps.api.v2.views.credentials_throttle_handler',
 }
+
+# Django-ratelimit Settings
+RATELIMIT_VIEW = 'credentials.apps.records.views.rate_limited'
 
 # DJANGO DEBUG TOOLBAR CONFIGURATION
 # http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
