fbe5297740eb605e31d0104504ee1512f944bf66
adeelehsan <adeel.ehsan@arbisoft.com>
2019-09-17 19:11:20 +0500

    Changed from_address while sending email to partner
    previously we were using learner-records@edx.org in from address.
    Now we will be using learner eamil address so that partner can
    contact with learner.
    
    Prod-624

--- credentials/apps/records/views.py
+++ credentials/apps/records/views.py
@@ -417,8 +417,7 @@ class ProgramSendView(LoginRequiredMixin, RatelimitMixin, RecordsEnabledMixin, V
         record_path = reverse('records:public_programs', kwargs={'uuid': public_record.uuid.hex})
         record_link = request.build_absolute_uri(record_path)
         csv_link = urllib.parse.urljoin(record_link, "csv")
-
-        msg = ProgramCreditRequest(request.site).personalize(
+        msg = ProgramCreditRequest(request.site, user.email).personalize(
             recipient=Recipient(username=None, email_address=pathway.email),
             language=certificate.language,
             user_context={
