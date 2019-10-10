257bb9df6543bf504a6e8c86d8903cc88f3b18b2
Simon Chen <schen@edx.org>
2019-10-10 13:01:32 -0400

    Add better admin table data for search and viewing

--- credentials/apps/catalog/admin.py
+++ credentials/apps/catalog/admin.py
@@ -2,8 +2,33 @@ from django.contrib import admin
 
 from credentials.apps.catalog.models import Course, CourseRun, Organization, Pathway, Program
 
-admin.site.register(Course)
-admin.site.register(CourseRun)
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('key', 'uuid', 'title')
+    list_filter = ('site',)
+    readonly_fields = ('uuid',)
+    search_fields = ('key', 'title', 'uuid')
+
+
+@admin.register(CourseRun)
+class CourseRunAdmin(admin.ModelAdmin):
+    list_display = ('key', 'uuid', 'title_override', 'start', 'end')
+    readonly_fields = ('uuid',)
+    search_fields = ('key', 'title_override', 'uuid')
+
+
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ('title', 'uuid', 'type')
+    readonly_fields = ('uuid',)
+    search_fields = ('title', 'uuid')
+
+
+@admin.register(Pathway)
+class PathwayAdmin(admin.ModelAdmin):
+    list_display = ('name', 'org_name', 'pathway_type', 'email', 'uuid')
+    readonly_fields = ('uuid',)
+    search_fields = ('name', 'uuid')
+
 admin.site.register(Organization)
-admin.site.register(Pathway)
-admin.site.register(Program)
