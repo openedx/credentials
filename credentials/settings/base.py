43be9eb3cf3dd202e97029a32af08bcbb6913f62
syedimranhassan <45480841+syedimranhassan@users.noreply.github.com>
2020-01-29 16:55:27 +0500

    Move default variables from config repo (#725)

--- credentials/settings/base.py
+++ credentials/settings/base.py
@@ -107,6 +107,7 @@ DATABASES = {
         'HOST': 'localhost',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',  # Set to empty string for default.
         'ATOMIC_REQUESTS': False,
+        'CONN_MAX_AGE': 60,
     }
 }
 
