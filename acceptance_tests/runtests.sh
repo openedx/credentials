69090439574fa6fee8a323951825cb2cb6afbb66
Matt Tuchfarber <mtuchfarber@edx.org>
2020-10-05 21:36:40 -0400

    Fix always passing acceptance tests
    The acceptance tests were catching the exit code and always returning
    zero indicating success even if they weren't passing.

--- acceptance_tests/runtests.sh
+++ acceptance_tests/runtests.sh
@@ -15,16 +15,17 @@ export VERIFY_XSS="true"
 trap cleanup TERM EXIT
 cleanup()
 {
+    # catch existing exit code so we can return it after we shut down the servers
+    exit_code=$?
     trap - TERM EXIT
     [ -n "$RUNSERVER_PID" ] && kill "$RUNSERVER_PID" 2>/dev/null
-    exit 0
+    exit $exit_code
 }
 
 # Set QUICK=1 or similar if you are trying to manually iterate
 if [ -z "$QUICK" ]; then
     "$BASEDIR"/runserver.sh >/dev/null 2>/dev/null &
     RUNSERVER_PID=$!
-
     "$BASEDIR"/provision.sh || exit 1
 fi
 
