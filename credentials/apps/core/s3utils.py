a13dbb2d60cc5863795443061ca0c9134a948e37
Christie Rice <8483753+crice100@users.noreply.github.com>
2020-10-16 11:43:19 -0400

    MICROBA-628 Add boto3 utils

--- credentials/apps/core/s3utils.py
+++ credentials/apps/core/s3utils.py
@@ -5,12 +5,20 @@ from functools import partial
 
 from django.conf import settings
 from storages.backends.s3boto import S3BotoStorage
+from storages.backends.s3boto3 import S3Boto3Storage
 
+# TODO: remove once we've switched to boto3
 MediaS3BotoStorage = partial(
     S3BotoStorage,
     location=settings.MEDIA_ROOT.strip('/')
 )
 
+MediaS3Boto3Storage = partial(
+    S3Boto3Storage,
+    location=settings.MEDIA_ROOT.strip('/')
+)
+
+# TODO: remove once we've switched to boto3 (it doesn't seem to be used anywhere)
 StaticS3BotoStorage = partial(
     S3BotoStorage,
     location=settings.STATIC_ROOT.strip('/')
