f7a8ce675499efced5aecc5935c650ada641419f
Tim McCormack <tmccormack@edx.org>
2020-10-22 09:26:33 -0400

    ARCHBOM-1569: Add a mock toggles state endpoint (#855)
    As part of Architecture's observability work, we're adding endpoints to
    IDAs that can dump the current state of various toggles.  This is a mock
    endpoint that accomplishes two things: 1) confirm that we can require a
    staff JWT in another IDA than edxapp, and 2) allow us to test integration
    of multiple IDAs into toggle state reports before the edx-toggles
    djangoapp is ready for use.
    
    This can go away once edx-toggles is ready and integrated.

--- credentials/views.py
+++ credentials/views.py
@@ -2,6 +2,12 @@ from urllib.parse import urljoin
 
 from django.views.generic.base import RedirectView
 
+from edx_rest_framework_extensions.auth.jwt.authentication import JwtAuthentication
+from edx_rest_framework_extensions.permissions import IsStaff
+from rest_framework import permissions, views
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.response import Response
+
 
 class FaviconView(RedirectView):
     """
@@ -12,3 +18,38 @@ class FaviconView(RedirectView):
         if not site_configuration.homepage_url:
             return None
         return urljoin(site_configuration.homepage_url, '/favicon.ico')
+
+
+class MockToggleStateView(views.APIView):  # pragma: no cover
+    """
+    A mock endpoint showing that we can require a staff JWT in this IDA,
+    and allowing us to test integration of multiple IDAs into toggle state
+    reports (ARCHBOM-1569). This can go away once edx-toggles is ready and
+    integrated.
+    """
+    authentication_classes = (JwtAuthentication, SessionAuthentication,)
+    permission_classes = (permissions.IsAuthenticated, IsStaff,)
+
+    def get(self, request):
+        return Response({
+            "waffle_flags": [
+                {
+                    "name": "mock.flag",
+                    "class": "WaffleFlag",
+                    "module": "mock.core.djangoapps.fake",
+                    "code_owner": "platform-arch",
+                    "computed_status": "off"
+                }
+            ],
+            "waffle_switches": [],
+            "django_settings": [
+                {
+                    "name": "MOCK_DEBUG",
+                    "is_active": False
+                },
+                {
+                    "name": "OTHER_MOCK['stuff']",
+                    "is_active": True
+                }
+            ]
+        })
