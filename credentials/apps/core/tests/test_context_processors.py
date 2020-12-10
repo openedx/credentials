59b02e2f133885b4a2bd4784c457725c007ef39a
Albert (AJ) St. Aubin <astaubin@edx.org>
2020-12-10 14:54:19 -0500

    [MICROBA-744] Update the Open edX Logo URL and config param

--- credentials/apps/core/tests/test_context_processors.py
+++ credentials/apps/core/tests/test_context_processors.py
@@ -24,7 +24,7 @@ class CoreContextProcessorTests(TestCase):
             'language_code': LANGUAGE_CODE,
             'platform_name': site_configuration.platform_name,
             'site_logo_url': getattr(settings, 'LOGO_URL', ''),
-            'openedx_logo_url': getattr(settings, 'LOGO_OPEN_EDX_URL', ''),
+            'openedx_logo_url': getattr(settings, 'LOGO_POWERED_BY_OPEN_EDX_URL', ''),
             'favicon_url': getattr(settings, 'FAVICON_URL', ''),
         }
 
