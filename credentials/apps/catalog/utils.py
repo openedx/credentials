561e76fad975ecaa7c3d466652a617cde6bde704
Matt Tuchfarber <mtuchfarber@edx.org>
2021-03-18 16:58:44 -0400

    Add deletions to copy_catalog command
    The copy_catalog management command now had optional deletions if you
    pass the `--delete` flag. This will propogate Discovery deletions into
    Credentials.

--- credentials/apps/catalog/utils.py
+++ credentials/apps/catalog/utils.py
@@ -1,30 +1,167 @@
 """Utilities for integration with the catalog service."""
+import logging
+from urllib.parse import urljoin
 
 from django.db import transaction
 
 from credentials.apps.catalog.models import Course, CourseRun, Organization, Pathway, Program
 
 
-# Note: these parsing functions don't attempt to clear out old data that is no longer provided by Discovery.
-# That shouldn't normally happen (ideally content just gets archived, not deleted).
-# Having broken links in our DB would be hard to deal with, so for now, we just leave any old objects around
-# and don't worry about matching current Discovery state exactly.
+logger = logging.getLogger(__name__)
 
 
-def parse_organization(site, data):
-    org, _ = Organization.objects.update_or_create(
+class CatalogDataSynchronizer:
+    COURSE = "course"
+    COURSE_RUN = "course_run"
+    ORGANIZATION = "organization"
+    PATHWAY = "pathway"
+    PROGRAM = "program"
+
+    def __init__(self, site, api_client, catalog_api_url, page_size):
+        self.site = site
+        self.api_client = api_client
+        self.catalog_api_url = catalog_api_url
+        self.page_size = page_size
+        self.existing_data = {
+            self.COURSE: Course.objects.filter(site=site),
+            self.COURSE_RUN: CourseRun.objects.filter(course__site=site),
+            self.ORGANIZATION: Organization.objects.filter(site=site),
+            self.PATHWAY: Pathway.objects.filter(site=site),
+            self.PROGRAM: Program.objects.filter(site=site),
+        }
+        self.updated_data_sets = {
+            self.COURSE: set(),
+            self.COURSE_RUN: set(),
+            self.ORGANIZATION: set(),
+            self.PATHWAY: set(),
+            self.PROGRAM: set(),
+        }
+        self.existing_data_sets = {
+            model: set(queryset.values_list("uuid", flat=True)) for (model, queryset) in self.existing_data.items()
+        }
+
+    def add_item(self, model_type, value):
+        self.updated_data_sets[model_type].add(value)
+
+    def fetch_data(self):
+        logger.info(f"Copying catalog data for site {self.site.domain}")
+        self._fetch_programs()
+        logger.info("Finished copying programs.")
+        self._fetch_pathways()
+        logger.info("Finished copying pathways.")
+        self._log_changes()
+
+    def remove_externally_deleted_data(self):
+        """Removes data that was deleted out of the Discovery service"""
+        for model_type in self.existing_data.keys():
+            removed = self.existing_data_sets[model_type] - self.updated_data_sets[model_type]
+            if removed:
+                logger.info(f"Removing the following {model_type} UUIDs: {removed}")
+                self.existing_data[model_type].filter(uuid__in=removed).delete()
+
+    def _fetch_programs(self):
+        programs_url = urljoin(self.catalog_api_url, "programs/")
+        next_page = 1
+        while next_page:
+            response = self.api_client.get(
+                programs_url, params={"exclude_utm": 1, "page": next_page, "page_size": self.page_size}
+            )
+            response.raise_for_status()
+            programs = response.json()
+            for program in programs["results"]:
+                logger.info(f'Copying program "{program["title"]}"')
+                parse_program(self.site, program, self)
+
+            next_page = next_page + 1 if programs["next"] else None
+
+    def _fetch_pathways(self):
+        api_client = self.api_client
+        pathways_url = urljoin(self.catalog_api_url, "pathways/")
+        next_page = 1
+        while next_page:
+            response = self.api_client.get(
+                pathways_url, params={"exclude_utm": 1, "page": next_page, "page_size": self.page_size}
+            )
+            response.raise_for_status()
+            pathways = response.json()
+            for pathway in pathways["results"]:
+                logger.info(f'Copying pathway "{pathway["name"]}"')
+                parse_pathway(self.site, pathway, self)
+            next_page = next_page + 1 if pathways["next"] else None
+
+    def _log_changes(self):
+        """Prints out the what data will be added and what will be deleted"""
+        logger.info("The copy_catalog command caused the following changes:")
+        for model_type in self.existing_data.keys():
+            added = [str(_uuid) for _uuid in self.updated_data_sets[model_type] - self.existing_data_sets[model_type]]
+            to_be_removed = [
+                str(_uuid) for _uuid in self.existing_data_sets[model_type] - self.updated_data_sets[model_type]
+            ]
+            if added:
+                logger.info(f"{model_type} UUIDs added: {added}")
+            if to_be_removed:
+                logger.info(f"{model_type} UUIDs to be removed: {to_be_removed if to_be_removed else None}")
+
+
+@transaction.atomic
+def parse_program(site, data, synchronizer=None):
+    program, _ = Program.objects.update_or_create(
+        site=site,
+        uuid=data["uuid"],
+        defaults={
+            "title": data["title"],
+            "type": data["type"],
+            "status": data["status"],
+            "type_slug": data["type_attrs"]["slug"],
+            "total_hours_of_effort": data["total_hours_of_effort"],
+        },
+    )
+    if synchronizer:
+        synchronizer.add_item(synchronizer.PROGRAM, program.uuid)
+
+    program.authoring_organizations.clear()
+    for org_data in data["authoring_organizations"]:
+        org = parse_organization(site, org_data, synchronizer)
+        program.authoring_organizations.add(org)
+
+    program.course_runs.clear()
+    for course_data in data["courses"]:
+        _, course_runs = parse_course(site, course_data, synchronizer)
+        program.course_runs.add(*course_runs)
+
+    return program
+
+
+@transaction.atomic
+def parse_course(site, data, synchronizer=None):
+    course, _ = Course.objects.update_or_create(
         site=site,
         uuid=data["uuid"],
         defaults={
             "key": data["key"],
-            "name": data["name"],
-            "certificate_logo_image_url": data["certificate_logo_image_url"],
+            "title": data["title"],
         },
     )
-    return org
+
+    if synchronizer:
+        synchronizer.add_item(synchronizer.COURSE, course.uuid)
+
+    course.owners.clear()
+    for org_data in data["owners"]:
+        org = parse_organization(site, org_data, synchronizer)
+        course.owners.add(org)
+
+    course_runs = []
+    for run_data in data["course_runs"]:
+        course_run = parse_course_run(course, run_data, synchronizer)
+        course.course_runs.add(course_run)
+        course_runs.append(course_run)
+
+    # We count course_runs separately, since some programs may not have all runs that a course does
+    return course, course_runs
 
 
-def parse_course_run(course, data):
+def parse_course_run(course, data, synchronizer=None):
     course_run, _ = CourseRun.objects.update_or_create(
         course=course,
         uuid=data["uuid"],
@@ -40,64 +177,28 @@ def parse_course_run(course, data):
             "end_date": data["end_date"] if "end_date" in data else data["end"],
         },
     )
+    if synchronizer:
+        synchronizer.add_item(synchronizer.COURSE_RUN, course_run.uuid)
     return course_run
 
 
-@transaction.atomic
-def parse_course(site, data):
-    course, _ = Course.objects.update_or_create(
+def parse_organization(site, data, synchronizer=None):
+    org, _ = Organization.objects.update_or_create(
         site=site,
         uuid=data["uuid"],
         defaults={
             "key": data["key"],
-            "title": data["title"],
+            "name": data["name"],
+            "certificate_logo_image_url": data["certificate_logo_image_url"],
         },
     )
-
-    course.owners.clear()
-    for org_data in data["owners"]:
-        org = parse_organization(site, org_data)
-        course.owners.add(org)
-
-    course_runs = []
-    for run_data in data["course_runs"]:
-        course_run = parse_course_run(course, run_data)
-        course.course_runs.add(course_run)
-        course_runs.append(course_run)
-
-    # We count course_runs separately, since some programs may not have all runs that a course does
-    return course, course_runs
+    if synchronizer:
+        synchronizer.add_item(synchronizer.ORGANIZATION, org.uuid)
+    return org
 
 
 @transaction.atomic
-def parse_program(site, data):
-    program, _ = Program.objects.update_or_create(
-        site=site,
-        uuid=data["uuid"],
-        defaults={
-            "title": data["title"],
-            "type": data["type"],
-            "status": data["status"],
-            "type_slug": data["type_attrs"]["slug"],
-            "total_hours_of_effort": data["total_hours_of_effort"],
-        },
-    )
-
-    program.authoring_organizations.clear()
-    for org_data in data["authoring_organizations"]:
-        org = parse_organization(site, org_data)
-        program.authoring_organizations.add(org)
-
-    program.course_runs.clear()
-    for course_data in data["courses"]:
-        _, course_runs = parse_course(site, course_data)
-        program.course_runs.add(*course_runs)
-
-    return program
-
-
-@transaction.atomic
-def parse_pathway(site, data):
+def parse_pathway(site, data, synchronizer=None):
     """
     Assumes that the associated programs were parsed before this is run.
     """
@@ -111,6 +212,9 @@ def parse_pathway(site, data):
             "pathway_type": data["pathway_type"],
         },
     )
+
+    if synchronizer:
+        synchronizer.add_item(synchronizer.PATHWAY, pathway.uuid)
 
     pathway.programs.clear()
     for program_data in data["programs"]:
