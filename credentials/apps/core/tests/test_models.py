d7db6bedecc595a2d675322f39918c489be86ebf
alangsto <46360176+alangsto@users.noreply.github.com>
2021-08-11 07:22:33 -0500

    feat: use verified name on program certificate (#1257)
    MST-895. If user has chosen to use their verified name on certificates,
    use the verified name to render a program certificate.

--- credentials/apps/core/tests/test_models.py
+++ credentials/apps/core/tests/test_models.py
@@ -59,24 +59,67 @@ class SiteConfigurationTests(SiteMixin, TestCase):
         expected = "{}/api/user/v1/".format(self.site_configuration.lms_url_root.strip("/"))
         self.assertEqual(self.site_configuration.user_api_url, expected)
 
+    def test_name_verification_api_url(self):
+        """Verify the Name Verification API URL is composed correctly."""
+        expected = "{}/api/edx_name_affirmation/v1/verified_name".format(
+            self.site_configuration.lms_url_root.strip("/")
+        )
+        self.assertEqual(self.site_configuration.name_verification_api_url, expected)
+
     @responses.activate
     @override_settings(USER_CACHE_TTL=60)
-    def test_get_user_api_data(self):
+    def test_get_user_api_data_with_cache(self):
         """Verify the method retrieves data from the User API and caches it."""
         username = Faker().user_name()
-        data = {
+        user_data = {
             "username": username,
         }
-        url = f"{self.site_configuration.user_api_url}accounts/{username}"
-        responses.add(responses.GET, url, body=json.dumps(data), content_type=JSON, status=200)
+        user_url = f"{self.site_configuration.user_api_url}accounts/{username}"
+        responses.add(responses.GET, user_url, body=json.dumps(user_data), content_type=JSON, status=200)
+
+        verification_data = {
+            "username": "jdoe",
+            "verified_name": "Jonathan Doe",
+            "profile_name": "Jon Doe",
+            "verification_attempt_id": 123,
+            "proctored_exam_attempt_id": None,
+            "is_verified": True,
+            "use_verified_name_for_certs": False,
+            "verified_name_enabled": True,
+        }
+        verification_url = f"{self.site_configuration.name_verification_api_url}?username={username}"
+        responses.add(
+            responses.GET, verification_url, body=json.dumps(verification_data), content_type=JSON, status=200
+        )
         self.mock_access_token_response()
 
+        expected_data = user_data
+        expected_data["verified_name"] = "Jonathan Doe"
+        expected_data["use_verified_name_for_certs"] = False
+
         actual = self.site_configuration.get_user_api_data(username)
-        self.assertEqual(actual, data)
-        self.assertEqual(len(responses.calls), 2)
+        self.assertEqual(actual, expected_data)
+        self.assertEqual(len(responses.calls), 3)
 
         # Verify the data is cached
         responses.reset()
         actual = self.site_configuration.get_user_api_data(username)
-        self.assertEqual(actual, data)
+        self.assertEqual(actual, expected_data)
         self.assertEqual(len(responses.calls), 0)
+
+    @responses.activate
+    def test_get_user_api_data_with_404(self):
+        """Verify the method does not throw exception when non 200 status is returned by verified name API"""
+        username = Faker().user_name()
+        user_data = {
+            "username": username,
+        }
+        user_url = f"{self.site_configuration.user_api_url}accounts/{username}"
+        responses.add(responses.GET, user_url, body=json.dumps(user_data), content_type=JSON, status=200)
+
+        verification_url = f"{self.site_configuration.name_verification_api_url}?username={username}"
+        responses.add(responses.GET, verification_url, content_type=JSON, status=404)
+        self.mock_access_token_response()
+
+        actual = self.site_configuration.get_user_api_data(username)
+        self.assertEqual(actual, user_data)
