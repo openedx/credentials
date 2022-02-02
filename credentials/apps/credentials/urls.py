f3e4eaa76aa4f929ed905f6b918ffc99eca1d881
Edx Deployment automation robot <edx-deployment@edx.org>
2022-02-02 18:25:25 +0000

    chore: Bump credentials-themes to 0.1.104
    ... and fix lint issues after `black` update
    
    Our latest round of Python dependency updates includes an update to Black, our code formatting tool used in the `credentials` repo. The updated version must have some updating formatting/linting rules that are now failing. This upgrade PR includes fixes to pass the quality and linting tests.

--- credentials/apps/credentials/urls.py
+++ credentials/apps/credentials/urls.py
@@ -9,6 +9,6 @@ from credentials.apps.credentials.constants import UUID_PATTERN
 
 urlpatterns = [
     re_path(r"^example/$", views.ExampleCredential.as_view(), name="example"),
-    re_path(fr"^example/{UUID_PATTERN}/$", views.RenderExampleProgramCredential.as_view(), name="render_example"),
-    re_path(fr"^{UUID_PATTERN}/$", views.RenderCredential.as_view(), name="render"),
+    re_path(rf"^example/{UUID_PATTERN}/$", views.RenderExampleProgramCredential.as_view(), name="render_example"),
+    re_path(rf"^{UUID_PATTERN}/$", views.RenderCredential.as_view(), name="render"),
 ]
