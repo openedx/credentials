b70ccdef84bd818ee445c974f0c6a8eca17f9257
Gonzalo Romero <53199845+NeOneSoft@users.noreply.github.com>
2022-06-28 17:52:07 -0500

    feat: modify  re_path from urls.py file
    Co-authored-by: Olivia Ruiz-Knott <oruiz-knott@edx.org>

--- credentials/urls.py
+++ credentials/urls.py
@@ -50,7 +50,7 @@ urlpatterns = oauth2_urlpatterns + [
     re_path(r"^admin/", admin.site.urls),
     re_path(r"^api/", include(("credentials.apps.api.urls", "api"), namespace="api")),
     re_path(r"^api-auth/", include((oauth2_urlpatterns, "rest_framework"), namespace="rest_framework")),
-    re_path(r"^api-docs/$", schema_view.with_ui('swagger', cache_timeout=0), name="api_docs"),
+    re_path(r"^api-docs/$", schema_view.with_ui("swagger", cache_timeout=0), name="api_docs"),
     re_path(r"^auto_auth/$", core_views.AutoAuth.as_view(), name="auto_auth"),
     re_path(r"^credentials/", include(("credentials.apps.credentials.urls", "credentials"), namespace="credentials")),
     re_path(r"^health/$", core_views.health, name="health"),
