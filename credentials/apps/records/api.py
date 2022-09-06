691202447ea56b7220a9e272821012ebed6f29c0
Justin Hynes <jhynes@edx.org>
2022-09-06 08:56:09 -0400

    feat: Add ProgramCertRecord data to API response for Learner Record MFE rendering decisions. (#1743)
    [APER-1946]
    
    Adds a new field to the dictionary response returned by the `get_record_data` function. The new data will be information about the shared ProgramCertRecord for a Program, if it exists. This data will be returned in an existing REST API to the Learner Record MFE where it will be used to conditionally render button states in an individual Program Record.
    
    * Move private function `_get_course_data` to the top of the file.
    * Updated code comments and docstrings to provide additional context wherever possible.
    * Refactors the existing logic that retrieves the data presented in our Program Record page in an attempt to reduce cognitive load. I found it very difficult to follow all the logic in the original mega-function. While this is not perfect, I think it's better than it was before.
    * Include the UUID of a shared program record if it exists
    * Add tests for refactored functions and new functionality

--- credentials/apps/records/api.py
+++ credentials/apps/records/api.py
@@ -15,95 +15,89 @@ from credentials.apps.credentials.data import UserCredentialStatus
 from credentials.apps.records.models import ProgramCertRecord, UserCreditPathway, UserGrade
 
 
-def get_record_data(user, program_uuid, site, platform_name=None):
-    """
-    Get all of the data associated with a record by its uuid
+COURSE_CERTIFICATE_CONTENT_TYPE = ContentType.objects.filter(app_label="credentials", model="coursecertificate")
 
-    Arguments:
-        user(user): User data from the request
-        program_uuid(str):  Program uuid to find by
-        site(site): Django site
-        platform_name(str): Name of the platform associated with the program record
+
+def _does_awarded_program_cert_exist_for_user(program, user):
+    """
+    A utility function that determines if a (Program) Certificate has been awarded to a learner in a specified program.
+
+    Args:
+        program (Program): Program object instance
+        user (User): Django User object
 
     Returns:
-        dict(record): A record of the course runs associated with the provided user information and program uuid
+        Bool: The returned value represents if the learner has an awarded certificate in the specified program
     """
-    program = get_program_and_course_details(program_uuid, site)
-    program_course_runs = program.course_runs.all()
-    program_course_runs_set = frozenset(program_course_runs)
-
-    # Get all pathway organizations and their statuses
-    program_pathways = program.pathways.all()
-    program_pathways_set = frozenset(program_pathways)
-    user_credit_pathways = (
-        UserCreditPathway.objects.select_related("pathway").filter(user=user, pathway__in=program_pathways_set).all()
-    )
-    user_credit_pathways_dict = {user_pathway.pathway: user_pathway.status for user_pathway in user_credit_pathways}
-    pathways = [(pathway, user_credit_pathways_dict.setdefault(pathway, "")) for pathway in program_pathways]
-
-    # Find program credential if it exists (indicates if user has completed this program)
     program_credential_query = get_user_credentials_by_id(
-        user.username, UserCredentialStatus.AWARDED.value, program_uuid
+        user.username, UserCredentialStatus.AWARDED.value, program.uuid
     )
+    return program_credential_query.exists()
 
-    # Get all of the user course-certificates associated with the program courses (including not AWARDED ones)
-    course_certificate_content_type = ContentType.objects.filter(app_label="credentials", model="coursecertificate")
 
-    course_user_credentials = get_user_credentials_by_content_type(
-        user.username, course_certificate_content_type, status=None
-    )
+def _get_transformed_learner_data(user):
+    """
+    A utility function that extracts user data from the learner's User instance. This data is used to render a piece of
+    the Program Record page.
 
-    # Maps course run key to the associated credential
-    user_credential_dict = {
-        user_credential.credential.course_id: user_credential for user_credential in course_user_credentials
-    }
-    # Maps credentials to visible_date datetimes (a date when the cert becomes valid)
-    visible_dates = get_credential_dates(course_user_credentials, True)
+    Args:
+        user (User): Django User object
 
-    # Get all (verified) user grades relevant to this program
-    course_grades = UserGrade.objects.select_related("course_run__course").filter(
-        username=user.username, course_run__in=program_course_runs_set, verified=True
-    )
-
-    # Keep track of number of attempts and best attempt per course
-    num_attempts_dict = defaultdict(int)
-    highest_attempt_dict = {}  # Maps course -> highest grade earned
-    last_updated = None
-
-    # Find the highest course cert grades for each course
-    for course_grade in course_grades:
-        course_run = course_grade.course_run
-        course = course_run.course
-        user_credential = user_credential_dict.get(course_run.key)
-        if user_credential is not None:
-            num_attempts_dict[course] += 1
-            visible_date = visible_dates[user_credential]
-            last_updated = max(filter(None, [visible_date, course_grade.modified, last_updated]))
-
-            # Update grade if grade is higher and part of awarded cert
-            if user_credential.status == UserCredentialStatus.AWARDED.value:
-                current = highest_attempt_dict.setdefault(course, course_grade)
-                if course_grade.percent_grade > current.percent_grade:
-                    highest_attempt_dict[course] = course_grade
-
-    last_updated = last_updated or datetime.datetime.today()
-
-    learner_data = {
+    Returns:
+        Dict: A dictionary containing a subset of the learner's User instance data (full name, username, email)
+    """
+    return {
         "full_name": user.get_full_name(),
         "username": user.username,
         "email": user.email,
     }
 
-    program_data = {
+
+def _get_transformed_program_data(program, user, highest_attempt_dict, last_updated):
+    """
+    A utility function that transforms Program and Program metadata into a dictionary. This data is used to render a
+    piece of the Program Record page.
+
+    Args:
+        program (Program): Program object instance
+        user (User): Django User object
+        highest_attempt_dict (Dict): A dict containing a mapping of a learner's courses and the highest grade achieved
+            in those courses
+        last_updated (DateTime): A DateTime instance representing the last time the Program Record was updated
+
+    Returns:
+        Dict: A dictionary representing a subset of Program and related metadata important  rendering a learner's
+            Program Record page
+    """
+    return {
         "name": program.title,
         "type": slugify(program.type),
         "type_name": program.type,
-        "completed": program_credential_query.exists(),
+        "completed": _does_awarded_program_cert_exist_for_user(program, user),
         "empty": not highest_attempt_dict,
         "last_updated": last_updated.isoformat(),
         "school": ", ".join(program.authoring_organizations.values_list("name", flat=True)),
     }
 
+
+def _get_transformed_pathway_data(program, user):
+    """
+    Gathers and transforms pathway data specific to a learner's status in a given Program.
+
+    Args:
+        program (Program): Program object instance
+        user (User): Django User object
+    """
+    program_pathways = program.pathways.all()
+    program_pathways_set = frozenset(program_pathways)
+
+    user_credit_pathways = (
+        UserCreditPathway.objects.select_related("pathway").filter(user=user, pathway__in=program_pathways_set).all()
+    )
+    # maps a learner's pathway status to a pathway
+    user_credit_pathways_dict = {user_pathway.pathway: user_pathway.status for user_pathway in user_credit_pathways}
+    pathways = [(pathway, user_credit_pathways_dict.setdefault(pathway, "")) for pathway in program_pathways]
+    # finally, convert the data to a list of dictionaries in the form desired
     pathway_data = [
         {
             "name": pathway[0].name,
@@ -114,41 +108,86 @@ def get_record_data(user, program_uuid, site, platform_name=None):
         }
         for pathway in pathways
     ]
-    # Add course-run data to the response in the order that is maintained by the Program's sorted field
-    course_data = _get_course_data(program_course_runs, highest_attempt_dict, user_credential_dict, num_attempts_dict)
 
-    return {
-        "learner": learner_data,
-        "program": program_data,
-        "platform_name": platform_name,
-        "grades": course_data,
-        "pathways": pathway_data,
-    }
+    return pathway_data
 
 
-def _get_course_data(program_course_runs, highest_attempt_dict, user_credential_dict, num_attempts_dict):
+def _get_transformed_grade_data(program, user):
     """
-    Get course run grade data from the program course runs
+    A utility function that gathers and transforms a learner's grade data for course-runs that are part of a Program.
+    This data is used to render a learner's Program Record page.
 
-    Arguments:
-        program_course_runs(QuerySet): List of program course runs
-        user_credential_dict(dict): Dict of user credentials
-        highest_attempt_dict(dict): Dict for keeping track of the best attempt
-        num_attempts_dict(dict): Dict for keeping track of number of attempts
+    Args:
+        program (Program): Program object instance
+        user (User): Django User object
 
     Returns:
-        list(course_data): List of course data
+        transformed_grade_data (Dict): A dictionary containing a subset of a learner's grade information for each
+            course-run of a specific Program
+        highest_attempt_dict (Dict): A dictionary mapping a learner's highest achieved grade in each course of a
+            specific Program
+        last_updated (DateTime): A DateTime representing the most recent time a learner's grades or certificates were
+            updated in a specific Program.
     """
-    # Add course-run data to the response in the order that is maintained by the Program's sorted field
-    course_data = []
+    # creates a Django QuerySet containing the course-runs of the specified program
+    program_course_runs = program.course_runs.all()
+    # creates an immutable set of course-run data from the above QuerySet
+    program_course_runs_set = frozenset(program_course_runs)
+
+    # get all of the learner's course certificates associated with the program courses (including non-AWARDED ones)
+    course_user_credentials = get_user_credentials_by_content_type(
+        user.username, COURSE_CERTIFICATE_CONTENT_TYPE, status=None
+    )
+    # create a new dictionary, mapping a course-run id (key) to an associated credential
+    user_credential_dict = {
+        user_credential.credential.course_id: user_credential for user_credential in course_user_credentials
+    }
+    # maps a credential to its visible_date (a date when the certificate becomes viewable)
+    visible_dates = get_credential_dates(course_user_credentials, True)
+    # retrieves the learner's grades (from verified course-runs) relevant to this program
+    course_grades = UserGrade.objects.select_related("course_run__course").filter(
+        username=user.username, course_run__in=program_course_runs_set, verified=True
+    )
+
+    # `num_attempts_dict` is used to track how many times a learner has attempted a particular course
+    num_attempts_dict = defaultdict(int)
+    # `highest_attempt_dict` maps the learner's highest grade earned in a course-run to a course
+    highest_attempt_dict = {}
+    # `last_updated` tracks the most recent time any certificate or grade was last updated
+    last_updated = None
+
+    # find the learner's highest grade for each course, how many times they have taken each course, and keep track of
+    # the last time these records were updated
+    for course_grade in course_grades:
+        course_run = course_grade.course_run
+        course = course_run.course
+
+        user_credential = user_credential_dict.get(course_run.key)
+        if user_credential:
+            num_attempts_dict[course] += 1
+            visible_date = visible_dates[user_credential]
+            last_updated = max(filter(None, [visible_date, course_grade.modified, last_updated]))
+
+            # find the highest course grade out of all attempts at a course
+            if user_credential.status == UserCredentialStatus.AWARDED.value:
+                current = highest_attempt_dict.setdefault(course, course_grade)
+                if course_grade.percent_grade > current.percent_grade:
+                    highest_attempt_dict[course] = course_grade
+
+    last_updated = last_updated or datetime.datetime.today()
+
+    # now, transform this grade data into the format we prefer for our response
+    transformed_grade_data = []
     added_courses = set()
+
+    # add the course-run grade data to teh response in the order that is maintained by the Program's sorted field
     for course_run in program_course_runs:
         course = course_run.course
         grade = highest_attempt_dict.get(course)
 
-        # If user hasn't taken this course yet, or doesn't have a cert, we want to show empty values
+        # if the learner hasn't taken this course yet, or doesn't have a cert, we want to show empty values
         if grade is None and course not in added_courses:
-            course_data.append(
+            transformed_grade_data.append(
                 {
                     "name": course.title,
                     "school": ", ".join(course.owners.values_list("name", flat=True)),
@@ -160,12 +199,10 @@ def _get_course_data(program_course_runs, highest_attempt_dict, user_credential_
                 }
             )
             added_courses.add(course)
-
-        # If the user has taken the course, show the course_run info for the highest grade
         elif grade is not None and grade.course_run == course_run:
             user_credential = user_credential_dict.get(course_run.key)
             issue_date = get_credential_dates(user_credential, False)
-            course_data.append(
+            transformed_grade_data.append(
                 {
                     "name": course_run.title,
                     "school": ", ".join(course.owners.values_list("name", flat=True)),
@@ -177,12 +214,73 @@ def _get_course_data(program_course_runs, highest_attempt_dict, user_credential_
                 }
             )
             added_courses.add(course)
-    return course_data
+
+    # In the current implementation of the Program Record page, the Program info is coupled to some of the grade data
+    # generated in this function. Previously, all this logic was in a single function and it was hard to follow. While
+    # we have refactored this function to be a bit more readable, I still need to pass back `highest_attempt_dict` and
+    # `last_updated` to keep some of the existing functionality working. Maybe in the future we get a chance to do a
+    # deeper refactor.
+    return transformed_grade_data, highest_attempt_dict, last_updated
+
+
+def _get_shared_program_cert_record_data(program, user):
+    """
+    A utility function that retrieves the UUID of a shared program record. This data is used to conditionally render
+    a component to share vs. copy a Program Record on our frontend.
+
+    Args:
+        program (Program): Program object instance
+        user (User): Django User object
+
+    Returns:
+        String: The UUID of the shared program record converted to a String
+    """
+    # if a ProgramCertRecord instance is found then return the UUID (as a string), otherwise return None
+    try:
+        shared_program_record = ProgramCertRecord.objects.get(user=user.id, program=program.id)
+    except ProgramCertRecord.DoesNotExist:
+        return None
+    else:
+        return str(shared_program_record.uuid)
+
+
+def get_program_record_data(user, program_uuid, site, platform_name=None):
+    """
+    Get all of the data associated with a record by its uuid
+
+    Arguments:
+        user(user): Django User object
+        program_uuid(str): A Program's unique indentifier, used to retrieve a Program object instance
+        site(site): Django site
+        platform_name(str): Name of the platform associated with the program record
+
+    Returns:
+        Dict: A dictionary containing a subset of learner, program, and grade information used to render our Program
+            Record page(s).
+    """
+    program = get_program_and_course_details(program_uuid, site)
+
+    learner_data = _get_transformed_learner_data(user)
+    grade_data, highest_attempt_dict, last_updated = _get_transformed_grade_data(program, user)
+    program_data = _get_transformed_program_data(program, user, highest_attempt_dict, last_updated)
+    pathway_data = _get_transformed_pathway_data(program, user)
+    shared_program_record_uuid = _get_shared_program_cert_record_data(program, user)
+
+    return {
+        "learner": learner_data,
+        "program": program_data,
+        "platform_name": platform_name,
+        "grades": grade_data,
+        "pathways": pathway_data,
+        "shared_program_record_uuid": shared_program_record_uuid,
+    }
 
 
 def get_program_details(request_user, request_site, uuid, is_public):
     """
-    Get details for a program using given ID
+    Retrieves the details (earned certificates and grade data) of a learner in the specified Program. This utility
+    function uses the value of `is_public` to determine if the `uuid` in the function arguments belongs to a Program or
+    a shared/public ProgramCertRecord instance.
 
     Arguments:
         request_user: User from the request (if not public)
@@ -190,10 +288,10 @@ def get_program_details(request_user, request_site, uuid, is_public):
         uuid(str): ID associated with this run of the program
         is_public(bool): Determines whether record can be sent/shared
     Returns:
-        A record associated with this program run
+        Dict: A dictionary of data that will be used to render the Program Record for a learner.
     """
-    # if a public view, the uuid is that of a ProgramCertRecord,
-    # if private, the uuid is that of a Program
+    # If public, the UUID is of a shared ProgramCertRecord instance. If private (is_public == false), the UUID is that
+    # of a specific Program.
     if is_public:
         program_cert_record = ProgramCertRecord.objects.get(uuid=uuid)
         user = program_cert_record.user
@@ -202,7 +300,9 @@ def get_program_details(request_user, request_site, uuid, is_public):
         user = request_user
         program_uuid = uuid
 
-    data = get_record_data(user, program_uuid, request_site, platform_name=request_site.siteconfiguration.platform_name)
+    data = get_program_record_data(
+        user, program_uuid, request_site, platform_name=request_site.siteconfiguration.platform_name
+    )
 
     site_configuration = request_site.siteconfiguration
     records_help_url = site_configuration.records_help_url if site_configuration else ""
