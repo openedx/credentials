dde8dc8306218058990a8fe1879477847bc854b7
Justin Hynes <JHynes@edx.org>
2022-10-19 15:50:42 -0400

    docs: Add comments on legacy frontend components/classes that can be removed as part of issue #1722
    [APER-1975]
    
    After releasing the Learner Record MFE we plan on starting removal of the legacy frontend components within the Credentials IDA. This PR just adds comments to the more obvious things which are clear for removal.

--- credentials/static/components/Analytics.jsx
+++ credentials/static/components/Analytics.jsx
@@ -1,3 +1,4 @@
+// TODO: We should be able to remove this as part of https://github.com/openedx/credentials/issues/1722
 // Function that wraps analytics.track so that it can be also be called directly
 function trackEvent(name, properties) {
   // Only load the key if it has been injected into the page already
--- credentials/static/components/RecordsHelp.jsx
+++ credentials/static/components/RecordsHelp.jsx
@@ -1,3 +1,4 @@
+// TODO: We should be able to remove this as part of https://github.com/openedx/credentials/issues/1722
 import React from 'react';
 import PropTypes from 'prop-types';
 import StringUtils from './Utils';
