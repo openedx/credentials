220e6867b70c82575069106b48e097a9cf5c2bc8
Awais Qureshi <awais.qureshi@arbisoft.com>
2023-05-16 20:45:11 +0500

    temp: just checking django42 behaviour.

--- credentials/apps/credentials/rest_api/v1/urls.py
+++ credentials/apps/credentials/rest_api/v1/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from credentials.apps.credentials.rest_api.v1.views import LearnerCertificateStatusView
 
 
 urlpatterns = [
-    url(
+    re_path(
         r"^learner_cert_status/$",
         LearnerCertificateStatusView.as_view(),
         name="learner_cert_status",
