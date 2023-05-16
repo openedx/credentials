6639cf6a7c30dae6cfa8ef41491bc4b5056da6d8
Awais Qureshi <awais.qureshi@arbisoft.com>
2023-05-16 20:34:07 +0500

    temp: just checking django42 behaviour.

--- credentials/apps/credentials/urls.py
+++ credentials/apps/credentials/urls.py
@@ -1,7 +1,7 @@
 """
 URLs for the credentials views.
 """
-from django.conf.urls import include, url
+from django.conf.urls import include, re_path
 from django.urls import re_path
 
 from credentials.apps.credentials import views
@@ -13,5 +13,5 @@ urlpatterns = [
     re_path(r"^example/$", views.ExampleCredential.as_view(), name="example"),
     re_path(rf"^example/{UUID_PATTERN}/$", views.RenderExampleProgramCredential.as_view(), name="render_example"),
     re_path(rf"^{UUID_PATTERN}/$", views.RenderCredential.as_view(), name="render"),
-    url(r"^api/", include((credentials_api_v1_urls, "api"), namespace="api")),
+    re_path(r"^api/", include((credentials_api_v1_urls, "api"), namespace="api")),
 ]
