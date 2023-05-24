a3dc6ff33d01938702b46c7d6bda19371673d616
Justin Hynes <jhynes@edx.org>
2023-05-24 12:55:12 -0400

    fix: unpin edx-event-bus-kafka and fix breaking changes (#2014)
    [APER-2438]
    
    - unpin `edx-event-bus-kafka` (ebk) and update dependencies to pull in latest version of ebk
    - remove the local `consume_events.py` as it is no longer needed
    - adds `openedx_events` as a third party app to Credentials

--- credentials/apps/credentials/management/commands/consume_events.py
+++
@@ -1,7 +0,0 @@
-"""
-Import the `consume_events` management command from the `event-bus-kafka` package in order to expose it here.
-
-This copies the approach currently used by the Discovery IDA.
-"""
-# pylint: disable=unused-import
-from edx_event_bus_kafka.management.commands.consume_events import Command
