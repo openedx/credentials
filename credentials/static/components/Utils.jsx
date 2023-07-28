680d76c75aecfd872702eda5f6284a1d21c8999c
Jason Wesson <jwesson@2u.com>
2023-07-28 23:36:32 +0000

    refactor: remove method from Utils

--- credentials/static/components/Utils.jsx
+++ credentials/static/components/Utils.jsx
@@ -1,14 +1,4 @@
 class StringUtils {
-  static interpolate(formatString, parameters) {
-    return formatString.replace(/{\w+}/g, (parameter) => {
-      const parameterName = parameter.slice(1, -1);
-      if (parameterName in parameters) {
-        return String(parameters[parameterName]);
-      }
-      return '{' + parameterName + '}';
-    });
-  }
-
   // Make a human readable string of string items concatenated with ','s and 'and'
   // e.g. 'first, second, third, and last' or 'first and second'
   static formatStringList(items) {
