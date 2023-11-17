dd8873fcaa74fbdbbd6032d51d6dd6927a630fc1
Justin Hynes <jhynes@edx.org>
2023-11-17 08:29:14 -0500

    feat: upgrade `django-ratelimit' to 4.x (#2297)

--- credentials/settings/base.py
+++ credentials/settings/base.py
@@ -98,7 +98,7 @@ MIDDLEWARE = (
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "social_django.middleware.SocialAuthExceptionMiddleware",
     "waffle.middleware.WaffleMiddleware",
-    "ratelimit.middleware.RatelimitMiddleware",
+    "django_ratelimit.middleware.RatelimitMiddleware",
     "edx_django_utils.cache.middleware.TieredCacheMiddleware",
     "edx_rest_framework_extensions.middleware.RequestMetricsMiddleware",
     "edx_rest_framework_extensions.auth.jwt.middleware.EnsureJWTAuthSettingsMiddleware",
